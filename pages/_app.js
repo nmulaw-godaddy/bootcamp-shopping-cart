40d9e8d5f2c15d77a244229cc7bf3432cbe1d4ce
jodugbesan-godadddy <jodugbesan@godaddy.com>
2026-07-16 15:17:47 -0700

    fix donut?

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 const { nextRedux } = require('../redux/store');
 import { AppCacheProvider } from '@mui/material-nextjs/v13-pagesRouter';
 import Layout from '../components/Layout';
 import { AuthProvider } from '../components/AuthContext';
+import LoadingDoughnut from '../components/LoadingDoughnut';
+import Box from '@mui/material/Box';
 import '../styles.css'
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
@@ -11,6 +13,23 @@ import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css'
 
 function WrappedApp({ Component, pageProps }) {
+  const [splash, setSplash] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => setSplash(false), 3000);
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (splash) {
+    return (
+      <AppCacheProvider>
+        <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', backgroundColor: 'rgb(12,51,84)' }}>
+          <LoadingDoughnut size={200} />
+        </Box>
+      </AppCacheProvider>
+    );
+  }
+
   return (
     <AppCacheProvider>
       <AuthProvider>
